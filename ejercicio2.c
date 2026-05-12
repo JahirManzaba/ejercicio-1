04a4497d0e50aad4803ea8c43f7d9f0140e9079b
JahirManzaba <jahir.manzaba@udla.edu.ec>
2026-05-12 11:42:34 -0500

    impresion con puntero

--- ejercicio2.c
+++ ejercicio2.c
@@ -2,9 +2,12 @@
 int main ()
 {
     char vocales[5] = {'a', 'e', 'i', 'o', 'u'};
- for (int i = 0; i < vocales; i++)
+    char *ptr;
+    ptr = &vocales[0];
+ for (int i = 0; i < 5; i++)
  {
 printf("%c", vocales[i]);
+print("%c\n", *(ptr+i));
  }
  
 }
