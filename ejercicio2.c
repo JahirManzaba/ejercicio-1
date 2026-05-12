015be8ccc9abfc94f039f5241b58f997c6efd506
JahirManzaba <jahir.manzaba@udla.edu.ec>
2026-05-12 11:49:10 -0500

    cambio de variable del arreglo

--- ejercicio2.c
+++ ejercicio2.c
@@ -4,10 +4,11 @@ int main ()
     char vocales[5] = {'a', 'e', 'i', 'o', 'u'};
     char *ptr;
     ptr = &vocales[0];
+    *(ptr+3)= 'x';
  for (int i = 0; i < 5; i++)
  {
 printf("%c", vocales[i]);
 print("%c\n", *(ptr+i));
  }
- 
+ return 0;
 }
