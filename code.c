1203737cf178517219b26e2d35f66effb3d09534
JahirManzaba <jahir.manzaba@udla.edu.ec>
2026-05-12 11:23:08 -0500

    segunda opcion de impresion del arreglos con punteros

--- code.c
+++ code.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main (){
-int arreglo[] = {2,4,6,8,10};
+
+int arreglo[] = {2, 4, 6, 8, 10};
 int *ptr;
 
 ptr = &arreglo[0];
 for (int i = 0; i < 5; i++)
 {
- printf("arreglo[%d] = %d",i, arreglo[i]);
- printf("%d", *ptr);
+ printf("arreglo[%d] = %d\t", i, arreglo[i]);
+// ptr = &arreglo[i];
+ printf("%d\n", *(ptr+i));
 }
 
 }
